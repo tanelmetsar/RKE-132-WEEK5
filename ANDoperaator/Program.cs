389e6fe57ad1eb62ddc55fc96e01a89147f4bbd6
tanelmetsar <tanel.metsar@tktk.ee>
2024-04-26 22:57:09 +0300

    add week5 code

---
+++ ANDoperaator/Program.cs
@@ -0,0 +1,19 @@
+﻿int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter you Chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congratiolations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application can not be approved.");
+}
---
+++ Week5/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Enter the temperature: ");
+int temp = Int32.Parse(Console.ReadLine());
+
+
+if (temp >= 30)
+{
+    Console.WriteLine("Boiling hot");
+}
+else if (temp < 30 && temp >= 20)
+{
+    Console.WriteLine("Hot");
+}
+else if (temp > 20 && temp >= 15)
+{
+    Console.WriteLine("Warm");
+}
+else if (temp < 15 && temp >= 10)
+{
+    Console.WriteLine("Chilly");
+}
+else if (temp < 10 && temp > 0)
+{
+    Console.WriteLine("Cold");
+}
+else
+{
+    Console.WriteLine("Freezing cold");
+}
